2fdc0944d8af37a91909bd207f0033f0dd5093f3
neeearina <arishschkarussia@gmail.com>
2023-04-11 12:16:55 +0500

    арина 2 изменение функция add

--- general_file_with_function.py
+++ general_file_with_function.py
@@ -1,8 +1,9 @@
 # здесь все функции для бота
 from telegram import ReplyKeyboardMarkup
+from telegram.ext import ConversationHandler
 
 reply_keyboard2 = [['/add'], ['/look_all']]
-markup2 = ReplyKeyboardMarkup(reply_keyboard2, one_time_keyboard=False, resize_keyboard=True)
+markup2 = ReplyKeyboardMarkup(reply_keyboard2, one_time_keyboard=True, resize_keyboard=True)
 
 
 async def help(update, context):
@@ -12,7 +13,7 @@ async def help(update, context):
 
 
 reply_keyboard1 = [['/help']]
-markup = ReplyKeyboardMarkup(reply_keyboard1, one_time_keyboard=False, resize_keyboard=True)
+markup = ReplyKeyboardMarkup(reply_keyboard1, one_time_keyboard=True, resize_keyboard=True)
 
 
 async def start(update, context):
@@ -21,8 +22,45 @@ async def start(update, context):
     await update.message.reply_text(data_read, reply_markup=markup)
 
 
-async def add(update, context):
-    pass
+reply_keyboard3 = [['/stop_add']]
+markup3 = ReplyKeyboardMarkup(reply_keyboard3, one_time_keyboard=True, resize_keyboard=True)
+
+
+async def add(update, context):  # команда пользователя, бот спрашивает про ссылку на товар с вб
+    await update.message.reply_text("Добавление нового товара для отслеивания.\n"
+                                    "\n"
+                                    "Отправь ссылку на товар из котолога Wildberries\n", reply_markup=markup3)
+    return 1
+
+
+async def ask_source(update, context):  # бот читает ссылку на товар и спрашивает о цене
+    source_product = update.message.text  # здесь ответ пользователя о ссылке на товар
+    print(source_product)
+    await update.message.reply_text(
+        f"Ссылка получена.\n"
+        f"\n"
+        f"Напиши цену, ниже которой надо оповестить тебя:)", reply_markup=markup3)
+    return 2
+
+
+async def ask_price(update, context):  # бот считывает цену, завершает диалог и добавляет все в бд
+    price_product = update.message.text
+    print(price_product)
+    await update.message.reply_text('Цена получена.\n'
+                                    'Товар добавлен в список отслеживаемых.\n'
+                                    'Мы оповестим тебя сразу, как только цена на товар упадёт!')
+    return ConversationHandler.END
+
+
+reply_keyboard4 = [['/add'], ['/look_all'],
+                   ['/help']]
+markup4 = ReplyKeyboardMarkup(reply_keyboard4, one_time_keyboard=True, resize_keyboard=True)
+
+
+async def stop_add(update, context):  # останавливает диалог и добавляет цену и товар в бд
+    await update.message.reply_text('Товар не добавлен в список отслеживаемых.\n'
+                                    'Выберите дальнейшие действия.', reply_markup=markup4)
+    return ConversationHandler.END
 
 
 async def look_all(update, context):
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # в этом файле вся основа приложения, регистрация команд, запуск приложения
 
 import logging
-from telegram.ext import Application, MessageHandler, filters
+from telegram.ext import Application, MessageHandler, filters, ConversationHandler
 from telegram.ext import ApplicationBuilder
 from telegram.ext import CommandHandler
 from general_file_with_function import *
@@ -18,9 +18,17 @@ def main():
     application = Application.builder().token("5853283682:AAH8OfYbD093o3_OAK2rMP-cCkR_8mIs9EE").build()
     application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("add", add))
     application.add_handler(CommandHandler("look_all", look_all))
+    conv_handler1 = ConversationHandler(
+        entry_points=[CommandHandler('add', add)],
+        states={
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, ask_source)], # бот спрашивает про ссылку на товар
+            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, ask_price)] # бот спрашивает про цену
+        },
+        fallbacks=[CommandHandler('stop_add', stop_add)] # полностью добавляет товар в бд
+    )
 
+    application.add_handler(conv_handler1)
     application.run_polling()
 
 
