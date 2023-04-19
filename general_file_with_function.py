401ad65600ff1e7e1048f144628901036383bb02
TolmachevValerij <98163361+TolmachevValerij@users.noreply.github.com>
2023-04-19 16:16:31 +0500

    Update general_file_with_function.py

--- general_file_with_function.py
+++ general_file_with_function.py
@@ -65,6 +65,9 @@ async def ask_source(update, context):  # бот читает ссылку на 
             f"Артикул на {sp[1]} действителен.\n"
             f"\n"
             f"Напиши цену, ниже которой надо оповестить тебя:)", reply_markup=markup3)
+        photo = open('photo_from_wb.jpg', 'rb')
+        photo = open('images/photo_from_wb.jpg', 'rb')
+        await context.bot.send_document(chat_id=chat_id, document=photo)
         return 2
     else:
         await update.message.reply_text('Артикул недействителен. Попробуйте еще раз.', reply_markup=markup4)
